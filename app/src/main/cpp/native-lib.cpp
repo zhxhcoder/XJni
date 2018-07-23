03a91b98e5bd3421780df884402aaee0b98ea968
zhxh <zhxhcoder@gmail.com>
2018-07-23 18:50:58 +0800

    SoManager

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -7,6 +7,6 @@ JNICALL
 Java_com_zhxh_xjni_SoManager_stringFromJNI(
         JNIEnv *env,
         jobject /* this */) {
-    std::string hello = "Hello from C++";
+    std::string hello = "Hello from SoManager Native";
     return env->NewStringUTF(hello.c_str());
 }
